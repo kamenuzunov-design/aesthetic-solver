fb70e33f48aa95dec74f03b9326745a61ca7ddec
kamenuzunov-design <kamen.uzunov@gmail.com>
2026-03-08 17:03:49 +0200

    Auto-update: нед 08.03.2026 г. 17:03:49,31

--- upload.bat
+++ upload.bat
@@ -2,10 +2,18 @@
 chcp 65001 >nul
 
 echo --- 1. Aktualizacia na GitHub ---
-git pull
+:: Насилствено се връщаме на main, за да избегнем detached HEAD
+git checkout main
+
+:: Указваме точно откъде и кой клон да се издърпа
+git pull origin main
+
 git add .
+:: Ако няма нищо за качване, Git ще прескочи commit-а без грешка
 git commit -m "Auto-update: %date% %time%"
-git push
+
+:: Качваме директно в main в GitHub
+git push origin main
 
 echo.
 echo --- 2. Deployment vav Firebase ---
